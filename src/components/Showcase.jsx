7ff704285554bbe5284bdc55bf818d9dcf7cf464
benjaminluan <benjaminluan1@gmail.com>
2022-08-17 15:52:21 -0400

    Calculator finished

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -2,29 +2,55 @@ import React, { useState } from "react";
 import "./Showcase.css";
 const Showcase = () => {
   const [displayValue, setDisplayValue] = useState(0);
-  const [currentValue, setCurrentValue] = useState(null);
+  const [currentValue, setCurrentValue] = useState(0);
   const [operator, setOperator] = useState(null);
   const [isOperator, setIsOperator] = useState(false);
+  const [activeOperatorClass, setActiveOperatorClass] = useState(null);
+
+  // Operator is now false, Operator is +, Current Value = 20, DisplayValue = 20.
 
   const handleClick = (input) => {
-    if (total.isOperator === false) {
-      setTotal((prevState) => {
-        return { ...prevState, displayValue: input };
-      });
-
-      console.log(total);
+    if (activeOperatorClass != null) {
+      setCurrentValue(displayValue);
+      setDisplayValue(input);
+      setActiveOperatorClass(null);
+    } else if (displayValue === 0) {
+      setDisplayValue(input);
+    } else {
+      setDisplayValue(displayValue + input);
     }
   };
 
   const handleOperator = (input) => {
-    if (isOperator === false && currentValue === null) {
+    if (isOperator === true && !operator) {
+      setDisplayValue(eval(`${currentValue}${operator}${displayValue}`));
+      setCurrentValue(eval(`${currentValue}${operator}${displayValue}`));
       setOperator(input);
+    } else if (isOperator === false) {
+      setOperator(input);
+      setIsOperator(true);
+    } else if (operator === operator) {
       setIsOperator(false);
-    } else {
-      
+      setOperator(null);
     }
+  };
 
-    console.log(total);
+  const handleEqualOperator = () => {
+    if (operator != null) {
+      setDisplayValue(eval(`${currentValue}${operator}${displayValue}`));
+      setOperator(null);
+      setIsOperator(false);
+      setCurrentValue(0);
+    }
+  };
+
+  const handleActiveOperatorClass = (input) => {
+    if (
+      activeOperatorClass === activeOperatorClass &&
+      activeOperatorClass != null
+    ) {
+      setActiveOperatorClass(null);
+    } else setActiveOperatorClass(input);
   };
 
   const buttonArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -43,19 +69,27 @@ const Showcase = () => {
                 <button
                   value={"CLEAR"}
                   className="calculator__button"
-                  onClick={() => setTotal((total.displayValue = 0))}
+                  onClick={() => {
+                    setDisplayValue(0);
+                    setCurrentValue(null);
+                    setActiveOperatorClass(null);
+                    setOperator(null);
+                    setIsOperator(false);
+                  }}
                 >
                   AC
                 </button>
                 <button
                   className="calculator__button"
-                  onClick={(e) => handleClick(e.target.textContent)}
+                  onClick={() => {
+                    setDisplayValue(displayValue * -1);
+                  }}
                 >
                   +/-
                 </button>
                 <button
                   className="calculator__button"
-                  onClick={(e) => handleClick(e.target.textContent)}
+                  onClick={() => setDisplayValue(displayValue / 100)}
                 >
                   %
                 </button>
@@ -75,39 +109,70 @@ const Showcase = () => {
                 </button>
                 <button
                   className="calculator__button gray"
-                  onClick={(e) => handleClick(e.target.textContent)}
+                  onClick={(e) => {
+                    displayValue - Math.floor(displayValue) == 0
+                      ? handleClick(e.target.textContent)
+                      : console.log("nope there's a decimal!");
+                  }}
                 >
                   .
                 </button>
               </div>
               <div className="calculator__buttons--right">
                 <button
-                  className="calculator__button"
-                  onClick={(e) => handleOperator(e.target.textContent)}
+                  className={
+                    "calculator__button" +
+                    (activeOperatorClass === "divide" ? " active" : "")
+                  }
+                  onClick={(e) => {
+                    handleOperator("/");
+                    handleActiveOperatorClass("divide");
+                  }}
                 >
                   &divide;
                 </button>
                 <button
-                  className="calculator__button"
-                  onClick={(e) => handleOperator(e.target.textContent)}
+                  className={
+                    "calculator__button" +
+                    (activeOperatorClass === "multiply" ? " active" : "")
+                  }
+                  onClick={(e) => {
+                    handleOperator("*");
+                    handleActiveOperatorClass("multiply");
+                  }}
                 >
                   &times;
                 </button>
                 <button
-                  className="calculator__button"
-                  onClick={(e) => handleOperator(e.target.textContent)}
+                  className={
+                    "calculator__button" +
+                    (activeOperatorClass === "subtract" ? " active" : "")
+                  }
+                  onClick={(e) => {
+                    handleOperator(e.target.textContent);
+                    handleActiveOperatorClass("subtract");
+                  }}
                 >
                   -
                 </button>
                 <button
-                  className="calculator__button"
-                  onClick={(e) => handleOperator(e.target.textContent)}
+                  className={
+                    "calculator__button" +
+                    (activeOperatorClass === "add" ? " active" : "")
+                  }
+                  onClick={(e) => {
+                    handleOperator(e.target.textContent);
+                    handleActiveOperatorClass("add");
+                  }}
                 >
                   +
                 </button>
                 <button
-                  className="calculator__button"
-                  onClick={(e) => handleOperator(e.target.textContent)}
+                  className={"calculator__button"}
+                  onClick={(e) => {
+                    handleEqualOperator(e.target.textContent);
+                    handleActiveOperatorClass("equal");
+                  }}
                 >
                   =
                 </button>
